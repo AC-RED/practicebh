16d99486d40288b57604289d8cfa5e3c151770d2
AC <AC@qq.com>
2019-12-18 19:46:48 +0800

    黑马头条PC-登录模块-数据绑定及校验

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,35 +1,76 @@
 <template>
   <div class="login">
     <el-card class="login-card">
-        <div class="title">
-            <img src="../../assets/img/logo_index.png" alt="">
-        </div>
-        <el-form style="margin-top:30px">
-            <el-form-item>
-                <el-input placeholder="请输入手机号"></el-input>
-            </el-form-item>
+      <div class="title">
+        <img src="../../assets/img/logo_index.png" alt />
+      </div>
+      <el-form style="margin-top:30px" :model="loginForm" :rules="loginRules" ref="myFrom">
+        <el-form-item prop='mobile'>
+          <el-input v-model="loginForm.mobile" placeholder="请输入手机号"></el-input>
+        </el-form-item>
 
-            <el-form-item>
-                <el-input placeholder="请输入验证码" style="width:65%"></el-input>
+        <el-form-item prop='code'>
+          <el-input v-model="loginForm.code" placeholder="请输入验证码" style="width:65%"></el-input>
 
-                <el-button plain style="float:right">获取验证码</el-button>
-            </el-form-item>
+          <el-button plain style="float:right">获取验证码</el-button>
+        </el-form-item>
 
-            <el-form-item>
-                <el-checkbox>我已阅读并同意用户协议和隐私条款</el-checkbox>
-            </el-form-item>
+        <el-form-item prop='check'>
+          <el-checkbox v-model="loginForm.check">我已阅读并同意用户协议和隐私条款</el-checkbox>
+        </el-form-item>
 
-            <el-form-item>
-                <el-button type="primary" style="width:100%">登录</el-button>
-            </el-form-item>
-        </el-form>
-
+        <el-form-item>
+          <el-button type="primary" style="width:100%" @click='submitLogin'>登录</el-button>
+        </el-form-item>
+      </el-form>
     </el-card>
   </div>
 </template>
 
 <script>
-export default {}
+export default {
+  data () {
+    return {
+      loginForm: {
+        mobile: '',
+        code: '',
+        check: false
+      },
+      loginRules: {
+        mobile: [{
+          required: true,
+          message: '请输入您的手机号'
+        }, {
+          pattern: /^1[3456789]\d{9}$/,
+          message: '请输入正确的手机号'
+        }],
+        code: [{
+          required: true,
+          message: '请输入您的验证码'
+        }, {
+          pattern: /^\d{6}$/,
+          message: '请输入正确的验证码'
+        }],
+        check: [{
+          validator: (rule, value, callback) => {
+            if (value) {
+              callback()
+            } else {
+              callback(new Error('请无条件同意该协定'))
+            }
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    submitLogin () {
+      this.$refs.myFrom.validate(function (allIn) {
+        if (allIn) {}
+      })
+    }
+  }
+}
 </script>
 
 <style lang='less' scoped>
@@ -44,10 +85,10 @@ export default {}
     width: 490px;
     height: 360px;
     .title {
-        text-align: center;
-        img {
-            height: 44px;
-        }
+      text-align: center;
+      img {
+        height: 44px;
+      }
     }
   }
 }
