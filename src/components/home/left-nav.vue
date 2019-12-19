664177e0efb8232605a9180fa20769ce393296e4
AC <AC@qq.com>
2019-12-19 18:01:29 +0800

    黑马头条PC-主页模块-左侧导航菜单

---
+++ src/components/home/left-nav.vue
@@ -0,0 +1,55 @@
+<template>
+<div class="left-nav">
+    <div class="title"><img src="../../assets/img/logo_admin.png" alt=""></div>
+
+    <el-aside>
+    <el-col>
+   <el-menu background-color='#353b4e' text-color='#adafb5' active-text-color='#ffd04b'>
+       <el-menu-item>
+        首页
+      </el-menu-item>
+
+      <el-submenu>
+          <span slot="title">内容管理</span>
+          <el-menu-item>发布文章</el-menu-item>
+          <el-menu-item>内容列表</el-menu-item>
+          <el-menu-item>评论列表</el-menu-item>
+          <el-menu-item>素材管理</el-menu-item>
+      </el-submenu>
+
+      <el-submenu>
+          <span slot="title">粉丝管理</span>
+          <el-menu-item>图文数据</el-menu-item>
+          <el-menu-item>粉丝概况</el-menu-item>
+          <el-menu-item>粉丝画像</el-menu-item>
+          <el-menu-item>粉丝列表</el-menu-item>
+      </el-submenu>
+
+      <el-menu-item>
+        账户信息
+      </el-menu-item>
+   </el-menu>
+  </el-col>
+  </el-aside>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang='less' scoped>
+.left-nav{
+    width: 230px;
+    .title{
+        background-color: #2e2f32;
+        text-align: center;
+        padding: 15px 0;
+        img {
+            height: 35px;
+        }
+    }
+}
+</style>
--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,16 +1,25 @@
 <template>
   <el-container>
-  <el-aside width="200px">Aside</el-aside>
+    <el-aside style="background-color:#353b4e;height:100vh;width:230px">
+      <left-nav></left-nav>
+    </el-aside>
+
   <el-container>
+
     <el-header>Header</el-header>
+
     <el-main>Main</el-main>
+
   </el-container>
 </el-container>
 </template>
 
 <script>
+import leftNav from '../../components/home/left-nav'
 export default {
-
+  components: {
+    'left-nav': leftNav
+  }
 }
 </script>
 
