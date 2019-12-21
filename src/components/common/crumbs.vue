9ae7b71fda792800772d5abf29b393a8e496dbf1
AC <AC@qq.com>
2019-12-21 17:48:02 +0800

    注册面包屑

---
+++ src/components/common/crumbs.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-breadcrumb>
+      <el-breadcrumb-item to="/home">首页</el-breadcrumb-item>
+
+      <el-breadcrumb-item name='title'><slot></slot></el-breadcrumb-item>
+  </el-breadcrumb>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/index.js
+++ src/components/index.js
@@ -1,9 +1,11 @@
 import leftNav from './home/left-nav'
 import subHead from './home/subject-header'
+import Crumbs from './common/crumbs'
 // 所有自定义组件的插件
 export default {
   install: function (Vue) {
     Vue.component('left-nav', leftNav) // 注册 左侧导航组件
     Vue.component('sub-header', subHead) // 注册头部组件
+    Vue.component('crumbs', Crumbs)// 注册面包屑
   }
 }
