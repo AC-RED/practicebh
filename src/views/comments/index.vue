9dc51ce15a077287aa8994d8ae762b9bb94559a9
AC <AC@qq.com>
2019-12-21 20:58:48 +0800

    黑马头条PC-评论列表-打开和关闭评论

--- src/views/comments/index.vue
+++ src/views/comments/index.vue
@@ -4,19 +4,45 @@
           <template slot="title">评论列表</template>
       </crumbs>
 
-      <el-table>
-          <el-table-column width="600" label="标题"></el-table-column>
-          <el-table-column label="评论状态"></el-table-column>
-          <el-table-column label="总评论数"></el-table-column>
-          <el-table-column label="粉丝评论数"></el-table-column>
-          <el-table-column label="操作"></el-table-column>
+      <el-table :data="list">
+          <el-table-column prop="title" width="600" label="标题"></el-table-column>
+          <el-table-column :formatter="fromatterBool" prop="comment_status" label="评论状态"></el-table-column>
+          <el-table-column prop="total_comment_count" label="总评论数"></el-table-column>
+          <el-table-column prop="fans_comment_count" label="粉丝评论数"></el-table-column>
+          <el-table-column label="操作">
+              <template slot-scope="obj">
+                  <el-button type="text" size="small">修改</el-button>
+                  <el-button type="text" size="small">{{obj.row.comment_status? '关闭评论':'打开评论'}}</el-button>
+              </template>
+
+          </el-table-column>
       </el-table>
   </el-card>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      list: []
+    }
+  },
+  methods: {
+    getComment () {
+      this.$axios({
+        url: '/articles',
+        params: { response_type: 'comment' }
+      }).then(result => {
+        this.list = result.data.results
+      })
+    },
+    fromatterBool (row, column, cellValue, index) {
+      return cellValue ? '正常' : '关闭'
+    }
+  },
+  created () {
+    this.getComment()
+  }
 }
 </script>
 
