d801626e84531230042be766b3d54f6ee51eacc6
AC <AC@qq.com>
2019-12-23 20:18:58 +0800

    黑马头条PC-素材列表-分页组件及请求

--- src/views/material/index.vue
+++ src/views/material/index.vue
@@ -1,29 +1,51 @@
 <template>
   <el-card>
-      <crumbs slot="header">
-          <template slot="title">素材管理</template>
-      </crumbs>
+    <crumbs slot="header">
+      <template slot="title">素材管理</template>
+    </crumbs>
 
-      <el-tabs v-model="activeName" @tab-click='changeTab'>
-          <el-tab-pane label="全部素材" name="all">
-              <div class="img-list">
-                  <el-card class="img-card" v-for="item in list" :key="item.id">
-                      <img :src="item.url" alt="">
-                      <el-row class="operate" type="flex" align="middle" justify="space-around">
-                          <i class="el-icon-star-on"></i>
-                          <i class="el-icon-delete-solid"></i>
-                      </el-row>
-                  </el-card>
-              </div>
-          </el-tab-pane>
-          <el-tab-pane label="收藏素材" name="collect">
-              <div class="img-list">
-                  <el-card class="img-card" v-for="item in list" :key="item.id">
-                      <img :src="item.url" alt="">
-                  </el-card>
-              </div>
-          </el-tab-pane>
-      </el-tabs>
+    <el-tabs v-model="activeName" @tab-click="changeTab">
+      <el-tab-pane label="全部素材" name="all">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+            <el-row class="operate" type="flex" align="middle" justify="space-around">
+              <i class="el-icon-star-on"></i>
+              <i class="el-icon-delete-solid"></i>
+            </el-row>
+          </el-card>
+        </div>
+
+        <el-row type="flex" justify="center" align="middle" style="height:80px;">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            :total="page.total"
+            @current-change="chengePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+      <el-tab-pane label="收藏素材" name="collect">
+        <div class="img-list">
+          <el-card class="img-card" v-for="item in list" :key="item.id">
+            <img :src="item.url" alt />
+          </el-card>
+        </div>
+
+        <el-row type="flex" justify="center" align="middle" style="height:80px;">
+          <el-pagination
+            background
+            layout="prev, pager, next"
+            :current-page="page.currentPage"
+            :page-size="page.pageSize"
+            :total="page.total"
+            @current-change="chengePage"
+          ></el-pagination>
+        </el-row>
+      </el-tab-pane>
+    </el-tabs>
   </el-card>
 </template>
 
@@ -32,19 +54,30 @@ export default {
   data () {
     return {
       activeName: 'all',
-      list: []
+      list: [],
+      page: {
+        total: 0,
+        pageSize: 12,
+        currentPage: 1
+      }
     }
   },
   methods: {
+    chengePage (newPage) {
+      this.page.currentPage = newPage
+      this.getAllMaterial()
+    },
     changeTab () {
+      this.page.currentPage = 1
       this.getAllMaterial()
     },
     getAllMaterial () {
       this.$axios({
         url: '/user/images',
-        params: { collect: this.activeName === 'collect' }
+        params: { collect: this.activeName === 'collect', page: this.page.currentPage, per_page: this.page.pageSize }
       }).then(result => {
         this.list = result.data.results
+        this.page.total = result.data.total_count
       })
     }
   },
@@ -56,26 +89,26 @@ export default {
 
 <style lang='less' scoped>
 .img-list {
-    display: flex;
-    flex-wrap: wrap;
-    .img-card{
-        width: 240px;
-        height: 200px;
-        margin: 20px 30px;
-        position: relative;
-        img{
-            width: 100%;
-            height: 100%;
-        }
-        .operate {
-            width: 100%;
-            height: 30px;
-            font-size: 20px;
-            position: absolute;
-            bottom: 0;
-            left: 0px;
-            background-color: rgb(248, 242, 242)
-        }
+  display: flex;
+  flex-wrap: wrap;
+  .img-card {
+    width: 240px;
+    height: 200px;
+    margin: 20px 30px;
+    position: relative;
+    img {
+      width: 100%;
+      height: 100%;
     }
+    .operate {
+      width: 100%;
+      height: 30px;
+      font-size: 20px;
+      position: absolute;
+      bottom: 0;
+      left: 0px;
+      background-color: rgb(248, 242, 242);
+    }
+  }
 }
 </style>
